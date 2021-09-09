29716aa41e0434838a0332b6f72cdd676683a249
OwenDrost <72380638+OwenDrost@users.noreply.github.com>
2021-09-09 16:48:36 +0800

    Add files via upload
    list 7z file
    eXtract 7z file

--- 7zbase.sh
+++ 7zbase.sh
@@ -13,6 +13,25 @@ PASSWD=$OUTFILE
 MDFILE=md-${OUTFILE}
 
 
+if [ "$1" = "x" ] ; then
+    FILENAME=$2
+    FILENAME=${FILENAME%*.7z}
+    # echo $FILENAME
+    SRCFILENAME=`echo ${FILENAME}|base64 -d`
+    echo $SRCFILENAME
+    7z x ${FILENAME}.7z -ry -p${FILENAME} 
+    rm -rf md-${FILENAME}
+    exit 1
+elif [ "$1" = "l" ] ; then
+    FILENAME=$2
+    FILENAME=${FILENAME%*.7z}
+    # echo $FILENAME
+    SRCFILENAME=`echo ${FILENAME}|base64 -d`
+    echo $SRCFILENAME
+    7z l ${FILENAME}.7z -ry -p${FILENAME} 
+    exit 1
+fi
+
 if [ "$#" -eq "1" ] ; then
     date +%s |sha256sum |base64 > ${MDFILE}
     7za a -t7z  -r -p${PASSWD} -mhe -mx0 ${OUTFILE}.7z ${FILENAME}  ${MDFILE}
@@ -33,6 +52,8 @@ else
     echo -e "\033[32me.g: \033[0m"
     echo -e "\033[32m   7zbase.sh file level passwd  \033[0m"
     echo -e "\033[32m   default 7zbase.sh file -mx0 -pfile  \033[0m"
+    echo -e "\033[32m   7zbase.sh x file ---eXtract 7z \033[0m"
+    echo -e "\033[32m   7zbase.sh l file ---list file  \033[0m"
 fi
 
 
